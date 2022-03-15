0783af76a059c688b354a73e57720d3ca41f9c9d
Anna Sakurai <zenasak@gmail.com>
2022-03-15 22:48:44 +0100

    create data.js. add featured products. add styling

---
+++ frontend/src/data.js
@@ -0,0 +1,46 @@
+const data = {
+  products: [
+    {
+      name: '100 CONTINUE',
+      slug: '100-continue',
+      category: 'Poster',
+      image: '/images/100.jpg',
+      price: 100,
+      countInStock: 10,
+      breed: 'Kaiken',
+      rating: 5,
+    },
+    {
+      name: '200 OK',
+      slug: '200-ok',
+      category: 'Poster',
+      image: '/images/200.jpg',
+      price: 100,
+      countInStock: 5,
+      breed: 'Kaiken',
+      rating: 4,
+    },
+    {
+      name: '301 MOVED PERMANENTLY',
+      slug: '301-moved-permanently',
+      category: 'Poster',
+      image: '/images/301.jpg',
+      price: 110,
+      countInStock: 3,
+      breed: 'Kaiken',
+      rating: 4.5,
+    },
+    {
+      name: '404 NOT FOUND',
+      slug: '404-not-found',
+      category: 'Poster',
+      image: '/images/404.jpg',
+      price: 150,
+      countInStock: 0,
+      breed: 'Kaiken',
+      rating: 4.8,
+    },
+  ],
+};
+
+export default data;
