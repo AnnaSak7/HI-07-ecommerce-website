3849665c758853c570709c29539a88ad15511cdf
Anna Sakurai <zenasak@gmail.com>
2022-03-15 21:42:20 +0100

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">HTTP DOG</a>
+      </header>
+      <main>List products</main>
+    </div>
+  );
+}
+
+export default App;
