673fe0719177b6c0700eb5c60fe6969c935beb2c
Anna Sakurai <zenasak@gmail.com>
2022-03-16 16:09:15 +0100

    set proxy in package / install axios / connect frontend and backend

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # HYPER ISLAND BRIEF 07
 
 MERN ECOMMERCE WEBSITE
+
+### FetchProducts From Backend
+
+1. set proxy in package.json (need to set the proxy in package.json to access to backend in the frontend)
+2. npm install axios
+3. use effect hook
+4. use reducer hook
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,12 +1,24 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import data from '../data';
+import axios from 'axios';
+import Button from '@mui/material/Button';
+//import data from '../data';
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/api/products');
+      setProducts(result.data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
       <h1>New Arrivals</h1>
       <div className="products">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <div className="product" key={product.slug}>
             <Link to={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name} />
@@ -18,7 +30,9 @@ const Home = () => {
               <p>
                 <strong>$ {product.price}</strong>
               </p>
-              <button>Add to cart</button>
+              <Button size="small" variant="outlined">
+                Add to cart
+              </Button>
             </div>
           </div>
         ))}
