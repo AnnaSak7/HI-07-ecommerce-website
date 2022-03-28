b1976cd0f72b5d75c441766740c88d4da4102bfd
Anna Sakurai <zenasak@gmail.com>
2022-03-28 09:11:30 +0200

    add isAdmin middleware. implement api to return summary data. install react-google-chats to display info.

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import expressAsyncHandler from 'express-async-handler';
 import Order from '../models/orderModel.js';
 import User from '../models/userModel.js';
+import Product from '../models/productModel.js';
 import { isAdmin, isAuth } from '../utils.js';
 
 const orderRouter = express.Router();
@@ -48,7 +49,25 @@ orderRouter.get(
         },
       },
     ]);
-    res.send({ users, orders });
+    const dailyOrders = await Order.aggregate([
+      {
+        $group: {
+          _id: { $dateToString: { format: '%Y-%m-%d', date: '$createdAt' } },
+          orders: { $sum: 1 },
+          sales: { $sum: '$totalPrice' },
+        },
+      },
+      { $sort: { _id: 1 } },
+    ]);
+    const productCategories = await Product.aggregate([
+      {
+        $group: {
+          _id: '$category',
+          count: { $sum: 1 },
+        },
+      },
+    ]);
+    res.send({ users, orders, dailyOrders, productCategories });
   })
 );
 
--- frontend/src/pages/DashboardPage.js
+++ frontend/src/pages/DashboardPage.js
@@ -3,6 +3,7 @@ import React, { useContext, useEffect, useReducer } from 'react';
 import { Card, Col, Row } from 'react-bootstrap';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
+import Chart from 'react-google-charts';
 import { Store } from '../Store';
 import getError from '../utils';
 
@@ -94,6 +95,40 @@ const DashboardPage = () => {
               </Card>
             </Col>
           </Row>
+          <div className="my-3">
+            <h2>Sales</h2>
+            {summary.dailyOrders.length === 0 ? (
+              <MessageBox>No Sale</MessageBox>
+            ) : (
+              <Chart
+                width="100%"
+                height="400px"
+                chartType="ColumnChart"
+                loader={<div>Loading Chart...</div>}
+                data={[
+                  ['Date', 'Sales'],
+                  ...summary.dailyOrders.map((x) => [x._id, x.sales]),
+                ]}
+              ></Chart>
+            )}
+          </div>
+          <div className="my-3">
+            <h2>Categories</h2>
+            {summary.productCategories.length === 0 ? (
+              <MessageBox>No Category</MessageBox>
+            ) : (
+              <Chart
+                width="100%"
+                height="400px"
+                chartType="PieChart"
+                loader={<div>Loading Chart...</div>}
+                data={[
+                  ['Category', 'Products'],
+                  ...summary.productCategories.map((x) => [x._id, x.count]),
+                ]}
+              ></Chart>
+            )}
+          </div>
         </>
       )}
     </div>
